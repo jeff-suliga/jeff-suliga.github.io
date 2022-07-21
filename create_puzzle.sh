e25cbbc1979b81b48b39cb37e74499800ddc67bf
Jeff Suliga <jeff.suliga@gmail.com>
2022-07-21 15:44:42 -0400

    10.2.1: Created script for new puzzles

--- create_puzzle.sh
+++ create_puzzle.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo hello world
+# Must be run from project's root folder
+
+read -p 'Puzzle Title (no spaces): ' PUZZLE_NAME
+read -p 'Puzzle Answer: ' PUZZLE_ANS
+
+touch Puzzles/$PUZZLE_NAME.md
+touch Puzzles/$PUZZLE_NAME-$PUZZLE_ANS.md
+
+cat Puzzles/Puzzle_Template.txt >> Puzzles/$PUZZLE_NAME.md
+cat Puzzles/Solution_Template.txt >> Puzzles/$PUZZLE_NAME-$PUZZLE_ANS.md
+
+if [[ ! -e images/$PUZZLE_NAME ]]
+then
+    echo 'Puzzle images folder not found. Creating folder now...'
+    mkdir images/$PUZZLE_NAME
+fi
