2f3868f00f417a566a4327bbe98fd81f9561dc25
Jeff Suliga <jeff.suliga7@gmail.com>
2023-05-19 12:12:50 -0400

    13.4.1: New puzzle ideas

---
+++ Puzzles/Ideas/test_words.py
@@ -0,0 +1,23 @@
+# ALPHA = [*'abcdefghijklmnopqrstuvwxyz']
+ALPHA = [*'qwertyuiop']
+
+def compute(word: str):
+    length = len(word)
+    if length < 5 or length > 9:
+        return False
+    
+    letters = []
+    for letter in word:
+        if letter in letters:
+            return False
+        elif letter not in ALPHA:
+            return False
+        letters.append(letter)
+        
+    return True
+
+with open('WordList.txt', 'r', encoding='utf-8') as file:
+    for line in file:
+        word = line.strip()
+        if compute(word):
+            print(word)
