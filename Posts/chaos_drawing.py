dd835d8521c3a0c085e01add3f4de0f123665fff
Jeff Suliga <jeff.suliga7@gmail.com>
2023-05-26 16:19:55 -0400

    13.5.6: Update chaos drawing script

--- Posts/chaos_drawing.py
+++ Posts/chaos_drawing.py
@@ -16,16 +16,20 @@ parser.add_argument('--distance', type=float, required=False,
 parser.add_argument('--window', type=int, required=False, nargs=4,
                     help='[TLx, TLy, BRx, BRy] \nxy coordinates for the top left and bottom right\ncorners of the MS Paint drawing window. ' +
                     'You will be\nprompted through an interactive stage to collect this\ninformation if not provided.')
-# parser.add_argument('--help', type=int, required=False, nargs=4)
 args = parser.parse_args()
 
+# Time sleeping between clicks of the color palette
+# This is necessary for the change of color to register
 COLORINTERVAL = 0.05
+
+# Assign Defaults
 COLORED = False
 POINTS = 1000
-DISTANCE = 0.5 # (points - 2) / (points - 1)
+DISTANCE = 0.5 # (vertices - 2) / (vertices - 1)
 PAINTWINDOW = [[0, 0], [0, 0]]
 GETWINDOW = True
 
+# Parse Args
 if args.points and args.points > 10 and args.points < 100000:
     POINTS = args.points
 else:
@@ -40,12 +44,13 @@ if args.window:
     GETWINDOW = False
     #                Top Left X       Top Left Y     Bottom Right X   Bottom Right Y
     PAINTWINDOW = [[args.window[0], args.window[1]], [args.window[2], args.window[3]]]
+
     # check if the inputted window is valid
-    
     if PAINTWINDOW[0][0] >= PAINTWINDOW[1][0] or PAINTWINDOW[0][1] >= PAINTWINDOW[1][1]:
         print('Invalid Window size supplied. Exiting...')
         exit()
 
+# Print variables for this instance
 print(f'Colored:  {COLORED}')
 print(f'Points:   {POINTS}')
 print(f'Distance: {DISTANCE}')
@@ -53,7 +58,7 @@ print(f'Window:   {PAINTWINDOW}')
 
 sleep(3) # Wait at the start of the program
 
-if GETWINDOW:
+if GETWINDOW: # Get paint window dimensions
     gui.alert('Hover over the top left corner of the drawing window after exiting this window')
     sleep(2)
     pos = gui.position()
@@ -70,15 +75,15 @@ if GETWINDOW:
 
 print(f'Current Paint window settings: \t{PAINTWINDOW}')
 
-def inGrid(x: float, y: float):
+def inGrid(x: float, y: float): # Check if a point lies within the paint window
     return (x >= PAINTWINDOW[0][0] and x <= PAINTWINDOW[1][0]) and (y >= PAINTWINDOW[0][1] and y <= PAINTWINDOW[1][1])
 
 COLORS = []
-STARTPOINTS = []
+VERTICES = []
 
 lastColor = -1 # cache previous color to speed up colored drawings by a lot
 def clickColor(color: int, clickX, clickY):
-    global lastColor
+    global lastColor # I feel like I shouldn't have to do this but aight
     if COLORED and color != lastColor:
         gui.click(x=COLORS[color][0], y=COLORS[color][1], clicks=2, interval=COLORINTERVAL)
         gui.click(x=clickX, y=clickY)
@@ -86,12 +91,12 @@ def clickColor(color: int, clickX, clickY):
     else:
         gui.click(x=clickX, y=clickY)
 
-def getVertices():
-    vertex = 0
-    while gui.confirm("Click OK to add another vertex, Cancel to continue") == "OK":
+def getVertices(): # Populate the screen with the vertices of the drawing
+    index = 0
+    while gui.confirm("Click OK to add another vertex, Cancel to continue.") == "OK":
         
         if gui.confirm("Hover over the position of this vertex.") == "Cancel":
-            gui.alert('Point skipped')
+            gui.alert('Point skipped.')
             continue
         sleep(1)
         pos = gui.position()
@@ -100,7 +105,7 @@ def getVertices():
         if not inGrid(x, y):
             gui.alert(f'({x}, {y}) is not in the paint grid! Skipping Point.')
             continue
-        STARTPOINTS.append((x, y))
+        VERTICES.append((x, y))
 
         if COLORED:
             gui.confirm("Hover over the color of this vertex.")
@@ -109,21 +114,21 @@ def getVertices():
             cx = pos[0]
             cy = pos[1]
             COLORS.append((cx, cy))
-            if vertex == 0:
+            if index == 0:
                 gui.click(cx, cy) # click on the paint window to bring it to the front
                 sleep(0.03)
-        clickColor(vertex, x, y)
-        vertex += 1
-    if len(STARTPOINTS) == 0:
-        gui.alert("You've selected 0 starting points! Exiting...")
+        clickColor(index, x, y)
+        index += 1
+    if len(VERTICES) == 0:
+        gui.alert("You've selected 0 vertices! Exiting...")
         exit()
 
 getVertices()
 
-if gui.confirm('Starting points drawn. Continue?') != "OK":
+if gui.confirm('Vertices drawn. Continue?') != "OK":
     exit()
 
-gui.alert('After clicking OK, move your mouse to any starting point within 2 seconds, and then DROP THE MOUSE')
+gui.alert('After clicking OK, move your mouse anywhere in the grid within 2 seconds, and then DROP THE MOUSE')
 
 sleep(2)
 start = gui.position()
@@ -142,10 +147,10 @@ newY = 0
 prevX = startX
 prevY = startY
 while count < POINTS:
-    randPoint = randint(0, len(STARTPOINTS) - 1)
-    newX = prevX + int((STARTPOINTS[randPoint][0] - prevX) * DISTANCE)
-    newY = prevY + int((STARTPOINTS[randPoint][1] - prevY) * DISTANCE)
-    clickColor(randPoint, newX, newY)
+    randVertex = randint(0, len(VERTICES) - 1)
+    newX = prevX + int((VERTICES[randVertex][0] - prevX) * DISTANCE)
+    newY = prevY + int((VERTICES[randVertex][1] - prevY) * DISTANCE)
+    clickColor(randVertex, newX, newY)
     count += 1
     prevX = newX
     prevY = newY
