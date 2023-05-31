ddcad820ecbdbd2e9f4f9ebc950e215e7a80b280
Jeff Suliga <jeff.suliga7@gmail.com>
2023-05-31 16:57:19 -0400

    13.7.1: Add progress bar to chaos script

--- Posts/chaos_drawing.py
+++ Posts/chaos_drawing.py
@@ -30,6 +30,7 @@ import argparse
 import pyautogui as gui
 from time import sleep
 from random import randint
+from datetime import datetime
 
 gui.PAUSE = 0.01
 
@@ -187,14 +188,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--colored', type=str, required=False,
                         help='True/False for if you want the picture colored based on vertices. This takes much longer than uncolored.\nDefault:\tFalse')
-    parser.add_argument('--points', type=int, required=False,
+    parser.add_argument('-p', '--points', type=int, required=False,
                         help='Amount of points to be plotted. More takes longer but will be a clearer picture.\nDefault:\t1000')
-    parser.add_argument('--distance', type=float, required=False,
+    parser.add_argument('-d', '--distance', type=float, required=False,
                         help='Number between 0 and 1. Distance to be traveled at each step towards the randomly selected vertex.\nDefault:\t0.5')
-    parser.add_argument('--window', type=int, required=False, nargs=4,
+    parser.add_argument('-w', '--window', type=int, required=False, nargs=4,
                         help='[TLx, TLy, BRx, BRy] \nxy coordinates for the top left and bottom right\ncorners of the MS Paint drawing window. ' +
                         'You will be prompted through an interactive stage to collect this information if not provided.')
-    parser.add_argument('--vertices', type=int, required=False, nargs='+',
+    parser.add_argument('-v', '--vertices', type=int, required=False, nargs='+',
                         help='[x1 y1 x2 y2 x3 y3 ...] \nxy coordinates of any starting vertices. These must all be within the MS Paint drawing window. ' +
                         'You will be prompted interactively to add these if this is omitted. ' +
                         'This flag must be accompanied by the --window flag, and will be disregarded if --colored is set to \'True\'')
@@ -284,8 +285,9 @@ if __name__ == "__main__":
         gui.alert(f'({startX}, {startY}) is not in the paint grid! Exiting...')
         exit(64)
 
-    gui.click(startX, startY) # Place first point
+    start = datetime.now()
 
+    gui.click(startX, startY) # Place first point
     # draw points
     count = 0
     newX = 0
@@ -300,5 +302,17 @@ if __name__ == "__main__":
         count += 1
         prevX = newX
         prevY = newY
+        
+        # print a progress bar
+        progress = round(count / POINTS * 100, 1)
+        print('\rImage Progress:\t[' + ('=' * (int(progress) // 2)) + ('.' * ((99 - int(progress)) // 2)) + f'] {progress}%', end='')
+
+    end = datetime.now()
+
+    print('\nDone!')
+
+    elapsed = end - start
+    elapsed_us = str(elapsed / 1000000).split('.')[1]
+    print(f'Elapsed time: {elapsed.seconds}.{elapsed_us}s')
 
     gui.alert('Done!')
